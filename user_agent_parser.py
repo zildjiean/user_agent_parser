d596b532d4d98577a6e7c32e2590efa1e1f5bcdb
zildjiean <piyapong-pp@hotmail.com>
2016-05-25 18:36:02 +0700

    create v.1

---
+++ user_agent_parser.py
@@ -0,0 +1,18 @@
+#!/usr/python
+import httpagentparser
+import sys
+
+#s = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"
+
+#s = sys.argv[1]
+
+with open('agent.txt','r') as agentFile:
+        for i in agentFile:
+                agent = httpagentparser.detect(i)
+                platform = str(agent["platform"]["name"])
+                version = str(agent["platform"]["version"])
+                #bot = str(agent["bot"])
+                browser = str(agent["browser"]["name"])
+                bversion = str(agent["browser"]["version"])
+
+                print "Platform : "+platform+" |  "+" Version : "+version+" |  "+" Browser type : "+browser+" |  "+" Browser Version : "+bversion
